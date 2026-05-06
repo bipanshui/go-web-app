65744437c66c8d6692a834b9b175dcedcac93bc5
bipanshui <bipanshukumar21@gmail.com>
2026-05-06 22:11:56 +0530

    feat: add shared site styling

--- main.go
+++ main.go
@@ -25,14 +25,28 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/contact.html")
 }
 
+func newMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("static"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.Redirect(w, r, "/home", http.StatusFound)
+			return
+		}
+		http.NotFound(w, r)
+	})
+	mux.HandleFunc("/home", homePage)
+	mux.HandleFunc("/courses", coursePage)
+	mux.HandleFunc("/about", aboutPage)
+	mux.HandleFunc("/contact", contactPage)
+
+	return mux
+}
+
 func main() {
-
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
-
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	err := http.ListenAndServe("0.0.0.0:8080", newMux())
 	if err != nil {
 		log.Fatal(err)
 	}
